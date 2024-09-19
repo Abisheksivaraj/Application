a26363dfdb17f4ef978e550dbb4d3c819ee249cc
abisheksivaraj <abisheksivaraj2001.com>
2024-09-19 22:19:26 +0530

    task completed

--- src/Components/applicantList.jsx
+++ src/Components/applicantList.jsx
@@ -18,38 +18,56 @@ const ApplicantList = () => {
 
   const handleSaveClick = () => {
     if (applicantName.trim() !== "") {
-      setApplicants([...applicants, { name: applicantName, documents: [] }]);
+      const newApplicants = [
+        ...applicants,
+        { name: applicantName, documents: [] },
+      ];
+      setApplicants(newApplicants);
       setApplicantName("");
       setIsPopupOpen(false);
+
+      if (newApplicants.length > currentPage * itemsPerPage) {
+        setCurrentPage(Math.ceil(newApplicants.length / itemsPerPage));
+      }
     }
   };
 
   const handleFileChange = (e) => {
+    const files = Array.from(e.target.files);
+    setFileToUpload(files); 
+  };
+
+  const handleSaveFileClick = () => {
     if (documentName.trim() !== "" && fileToUpload) {
       const updatedApplicants = [...applicants];
-      const files = Array.from(e.target.files);
       updatedApplicants[selectedApplicantIndex].documents.push({
         documentName,
-        files,
+        files: fileToUpload,
       });
       setApplicants(updatedApplicants);
       setDocumentName("");
       setFileToUpload(null);
+      setSelectedApplicantIndex(null);
     }
   };
 
-  const handleCancelClick = () => {
-    setIsPopupOpen(false);
-    setApplicantName("");
+  const handleCancelFileUpload = () => {
+    setDocumentName("");
+    setFileToUpload(null);
+    setSelectedApplicantIndex(null);
   };
 
   const handleDeleteClick = (index) => {
-    setApplicants(applicants.filter((_, i) => i !== index));
+    const updatedApplicants = applicants.filter((_, i) => i !== index);
+    setApplicants(updatedApplicants);
+    const totalPages = Math.ceil(updatedApplicants.length / itemsPerPage);
+    if (currentPage > totalPages) {
+      setCurrentPage(totalPages);
+    }
   };
 
   const handleUploadClick = (index) => {
     setSelectedApplicantIndex(index);
-    setFileToUpload(true);
   };
 
   const indexOfLastApplicant = currentPage * itemsPerPage;
@@ -72,66 +90,73 @@ const ApplicantList = () => {
   };
 
   return (
-    <div className="bg-slate-200 h-screen p-5">
-      <div className="flex flex-row items-center justify-between">
-        <h1 className="text-[1.5rem] font-medium">Applicant Profiles</h1>
+    <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 min-h-screen p-4 sm:p-6 md:p-8 lg:p-10">
+      <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between mb-5">
+        <h1 className="text-2xl sm:text-3xl font-bold text-white mb-4 sm:mb-0">Applicant Details</h1>
         <button
           onClick={handleButtonClick}
-          className="flex items-center gap-1 bg-gray-500 p-3 w-[10rem] rounded-md text-[1rem] font-medium text-white"
+          className="flex items-center gap-2 bg-yellow-400 hover:bg-yellow-300 transition-all p-3 rounded-lg shadow-md text-lg font-medium text-white"
         >
           <IoIosPersonAdd />
           Add Applicant
         </button>
       </div>
 
-      <div className="flex justify-center">
-        <hr className="bg-slate-400 mt-10 w-[90%] h-1" />
+      <div className="flex justify-center mb-10">
+        <hr className="bg-white opacity-40 w-full max-w-4xl h-1 rounded-full" />
       </div>
 
-      <div className="mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
         {currentApplicants.map((applicant, index) => (
           <div
             key={index}
-            className="bg-white p-5 rounded-lg shadow-md relative"
+            className="bg-white bg-opacity-80 p-4 sm:p-6 rounded-xl shadow-lg transform transition-transform hover:scale-105 relative"
           >
-            <h2 className="text-xl font-medium mb-2">{applicant.name}</h2>
+            <h2 className="text-xl sm:text-2xl font-semibold text-gray-800 mb-3">
+              {applicant.name}
+            </h2>
 
             {applicant.documents.length > 0 && (
-              <table className="w-full table-auto border-collapse">
-                <thead>
-                  <tr>
-                    <th className="border px-4 py-2 text-left">
-                      Document Name
-                    </th>
-                    <th className="border px-4 py-2 text-left">File</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {applicant.documents.map((doc, docIndex) => (
-                    <tr key={docIndex}>
-                      <td className="border px-4 py-2 text-green-500">
-                        {doc.documentName}
-                      </td>
-                      <td className="border px-4 py-2">
-                        {doc.files.map((file, fileIndex) => (
-                          <a
-                            key={fileIndex}
-                            href={URL.createObjectURL(file)}
-                            download={file.name}
-                            className="text-blue-500"
-                          >
-                            {file.name}
-                          </a>
-                        ))}
-                      </td>
+              <div className="overflow-x-auto">
+                <table className="w-full text-sm md:text-base">
+                  <thead>
+                    <tr className="bg-gray-100">
+                      <th className="border px-2 py-1 text-left text-gray-800 font-semibold">Document Name</th>
+                      <th className="border px-2 py-1 text-left text-gray-800 font-semibold">File(s)</th>
                     </tr>
-                  ))}
-                </tbody>
-              </table>
+                  </thead>
+                  <tbody>
+                    {applicant.documents.map((doc, docIndex) => (
+                      <tr
+                        key={docIndex}
+                        className={`${
+                          docIndex % 2 === 0 ? "bg-gray-300" : "bg-gray-200"
+                        } hover:bg-gray-300`}
+                      >
+                        <td className="border px-2 py-1 text-green-600 font-medium">
+                          {doc.documentName}
+                        </td>
+                        <td className="border px-2 py-1">
+                          {doc.files.map((file, fileIndex) => (
+                            <a
+                              key={fileIndex}
+                              href={URL.createObjectURL(file)}
+                              download={file.name}
+                              className="text-blue-600 hover:text-blue-800 font-medium mr-2"
+                            >
+                              {file.name}
+                            </a>
+                          ))}
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
             )}
 
-            {fileToUpload && selectedApplicantIndex === index && (
-              <div className="mb-4">
+            {selectedApplicantIndex === index && (
+              <div className="mt-4">
                 <input
                   type="text"
                   value={documentName}
@@ -147,14 +172,14 @@ const ApplicantList = () => {
                 />
                 <div className="flex gap-2">
                   <button
-                    onClick={handleFileChange}
-                    className="bg-gray-500 p-2 rounded-md text-white"
+                    onClick={handleSaveFileClick}
+                    className="bg-green-500 p-2 rounded-md text-white shadow-md hover:bg-green-600 transition-all"
                   >
                     Save
                   </button>
                   <button
-                    onClick={() => setFileToUpload(false)}
-                    className="bg-red-500 p-2 rounded-md text-white"
+                    onClick={handleCancelFileUpload}
+                    className="bg-red-500 p-2 rounded-md text-white shadow-md hover:bg-red-600 transition-all"
                   >
                     Cancel
                   </button>
@@ -164,7 +189,7 @@ const ApplicantList = () => {
 
             <button
               onClick={() => handleUploadClick(index)}
-              className="flex items-center gap-1 bg-blue-500 p-2 rounded-md text-white"
+              className="flex items-center gap-2 bg-blue-500 hover:bg-blue-400 transition-all p-2 rounded-md text-white mt-4"
             >
               <IoMdCloudUpload />
               Upload Document
@@ -180,20 +205,26 @@ const ApplicantList = () => {
         ))}
       </div>
 
-      <div className="flex justify-between items-center mt-4">
+      <div className="flex flex-col sm:flex-row sm:justify-between items-center mt-8 text-white">
         <button
           onClick={handlePrevPage}
-          className="bg-gray-500 p-2 rounded-md text-white"
+          className={`p-3 rounded-md text-white shadow-md ${
+            currentPage === 1 ? "bg-gray-400" : "bg-gray-600 hover:bg-gray-500"
+          }`}
           disabled={currentPage === 1}
         >
           Previous
         </button>
-        <span className="text-lg font-medium">
+        <span className="text-lg sm:text-xl font-medium mb-4 sm:mb-0">
           Page {currentPage} of {Math.ceil(applicants.length / itemsPerPage)}
         </span>
         <button
           onClick={handleNextPage}
-          className="bg-gray-500 p-2 rounded-md text-white"
+          className={`p-3 rounded-md text-white shadow-md ${
+            currentPage === Math.ceil(applicants.length / itemsPerPage)
+              ? "bg-gray-400"
+              : "bg-gray-600 hover:bg-gray-500"
+          }`}
           disabled={currentPage === Math.ceil(applicants.length / itemsPerPage)}
         >
           Next
@@ -202,29 +233,27 @@ const ApplicantList = () => {
 
       {isPopupOpen && (
         <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50">
-          <div className="bg-white p-5 rounded-md shadow-lg w-full max-w-md">
-            <h2 className="text-xl font-medium mb-4">Add New Applicant</h2>
+          <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-lg">
+            <h2 className="text-2xl font-bold mb-4">Add New Applicant</h2>
             <input
               type="text"
               value={applicantName}
               onChange={(e) => setApplicantName(e.target.value)}
-              placeholder="Enter applicant's name"
+              placeholder="Enter applicant name"
               className="border p-2 w-full rounded-md mb-4"
             />
-            <div className="flex gap-2">
-              <button
-                onClick={handleSaveClick}
-                className="bg-gray-500 p-2 rounded-md text-white"
-              >
-                Save
-              </button>
-              <button
-                onClick={handleCancelClick}
-                className="bg-red-500 p-2 rounded-md text-white"
-              >
-                Cancel
-              </button>
-            </div>
+            <button
+              onClick={handleSaveClick}
+              className="bg-green-500 p-3 rounded-md text-white shadow-md hover:bg-green-600 transition-all"
+            >
+              Save
+            </button>
+            <button
+              onClick={handleButtonClick}
+              className="bg-red-500 p-3 rounded-md text-white shadow-md hover:bg-red-600 transition-all ml-2"
+            >
+              Cancel
+            </button>
           </div>
         </div>
       )}
